ffd876545048a0de0947ee3e648c05b23ce8f6cc
hoodielive <hoodielive@gmail.com>
2018-07-04 19:16:14 -0400

    working with arrows

---
+++ indecision-app/src/playground/es6-arrow-function.js
@@ -0,0 +1,11 @@
+// Challenge - Use arrow functions 
+// getFirstname('Mike Smith') -> "Mike" 
+// Create regular arrow function
+// Create arrow function using shorthand syntax 
+
+const getFirstname = (firstname) => {
+    return firstname; 
+}
+
+console.log(getFirstname("Mike", "Smith")); 
+
