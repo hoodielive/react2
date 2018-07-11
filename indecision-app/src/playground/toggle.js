67bd12da1913d712598de840ab13cad2b8a73f79
hoodielive <hoodielive@gmail.com>
2018-07-11 07:49:13 -0400

    finished product of toggler

--- indecision-app/src/playground/toggle.js
+++ indecision-app/src/playground/toggle.js
@@ -1,13 +1,26 @@
+let visibility = false; 
+
+let toggleVisibility = () => {
+    visibility = !visibility; 
+    render(); 
+}; 
 
 const render = () => {
-    const toggler = () => {
+    const toggler = (
         <div> 
-            <h1> Visibility Toggle </h1>
-            <button>Hide details</button>
-            <p>Hey. These are some details you can now see!</p>
+            <h1>Visibility Toggle</h1>
+            <button onClick={toggleVisibility}>
+                {visibility ? 'Hide details' : 'Show details'} 
+            </button>
+            { visibility && (
+                <div>
+                    <p>Hey. These are some details you can now see!</p>
+                </div>
+            )} 
         </div>
-    } 
-}
+    );
 
-const appRoot = document.getElementById('toggler')
+    ReactDOM.render(toggler, document.getElementById('app')); 
+};
+
 render(); 
