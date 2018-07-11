81cf2a88a8c4054443b03ba931f0c9e264c59bc9
hoodielive <hoodielive@gmail.com>
2018-07-10 20:26:04 -0400

    this is my version of toggler

---
+++ indecision-app/src/playground/toggle.js
@@ -0,0 +1,13 @@
+
+const render = () => {
+    const toggler = () => {
+        <div> 
+            <h1> Visibility Toggle </h1>
+            <button>Hide details</button>
+            <p>Hey. These are some details you can now see!</p>
+        </div>
+    } 
+}
+
+const appRoot = document.getElementById('toggler')
+render(); 
