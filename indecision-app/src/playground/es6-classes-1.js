4f30ffa7ad7a55cf4bc1b2f2231f8de3f958070a
hoodielive <hoodielive@gmail.com>
2018-07-11 22:42:15 -0400

    expanding classes

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -1,5 +1,15 @@
 
 class Person {
-    
-
+    constructor(name = 'Anonoymous') {
+        this.name = name || 'test'; 
+    }
+    getGreeting() {
+        return 'hi!'; 
+    }
 }
+
+const me = new Person('Law');  
+console.log(me.getGreeting());
+
+const other = new Person(); 
+console.log(other.getGreeting); 
