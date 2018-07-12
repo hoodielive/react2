311075b896eca2801fc30282f1100488fe93914d
hoodielive <hoodielive@gmail.com>
2018-07-11 23:02:27 -0400

    tell me something good lol

---
+++ indecision-app/src/playground/es6-classes-1-homework.js
@@ -0,0 +1,12 @@
+class Person {
+    constructor(name = 'Anonymous', age = 0) {
+       this.name = name;  
+       this.age = age; 
+    }
+    getDescription() {
+        return ` ${this.name} is ${this.age} years old ` 
+    }
+}
+
+const daPerson = new Person('Larry', 27); 
+console.log(daPerson.getDescription()); 
