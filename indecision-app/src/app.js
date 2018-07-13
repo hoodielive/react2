79db2a62d886281a3df94b7afd74c97bef5ce47a
hoodielive <hoodielive@gmail.com>
2018-07-13 18:32:59 -0400

    app.js props created

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,10 +1,23 @@
 
+class IndecisionApp extends React.Component {
+    render() {
+        return(
+            <div>
+                <Header title="Test value"/>
+                <Action />
+                <Options />
+                <AddOption />
+            </div>
+        );
+    }
+}
 class Header extends React.Component {
     render() {
+        console.log(this.props);
         return (
             <div>
-            <h1>Indecision</h1>
-            <h2>Put your life in the hands of a computer</h2>
+                <h1>Indecision</h1>
+                <h2>Put your life in the hands of a computer</h2>
             </div>
         );
     }
@@ -24,7 +37,17 @@ class Options extends React.Component {
     render() {
         return(
             <div>
-                <h1>Rendering static content</h1>
+               <Option /> 
+            </div>
+        );
+    }
+}
+
+class Option extends React.Component {
+    render() { 
+        return( 
+            <div> 
+                Option Returned
             </div>
         );
     }
@@ -39,14 +62,6 @@ class AddOption extends React.Component {
         ); 
     } 
 }
-const jsx = (
-    <div> 
-        <Header />
-        <Action />
-        <Options />
-        <AddOption />
-    </div>
 
-); 
 
-ReactDOM.render(jsx, document.getElementById('app')); 
+ReactDOM.render(<IndecisionApp />, document.getElementById('app')); 
