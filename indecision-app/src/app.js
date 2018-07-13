a519c1d11e3bda6051b36d3f3f2a71a074f09760
hoodielive <hoodielive@gmail.com>
2018-07-13 07:46:20 -0400

    components

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,14 +1,50 @@
 
 class Header extends React.Component {
     render() {
-        return <p>This is from Header</p>; 
+        return (
+            <div>
+            <h1>Indecision</h1>
+            <h2>Put your life in the hands of a computer</h2>
+            </div>
+        );
     }
 }
 
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        );
+    }
+}
+
+class Options extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Rendering static content</h1>
+            </div>
+        );
+    }
+}
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>I am static content</p>
+            </div>
+        ); 
+    } 
+}
 const jsx = (
     <div> 
-        <h1> Title </h1>
         <Header />
+        <Action />
+        <Options />
+        <AddOption />
     </div>
 
 ); 
